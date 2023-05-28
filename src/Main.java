fb06a322ae2c5897ad91b7b2728d3b9e0e5aec89
visan <alexandruvisan44@yahoo.com>
2023-05-28 13:14:25 +0300

    Removed unecessary declaration.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import zoo.cli.MainMenu;
-import zoo.db.Database;
 
 public class Main {
     public static void main(String[] args) {
-        Database db = Database.getDatabase();
         MainMenu menu = new MainMenu();
         menu.render();
     }
